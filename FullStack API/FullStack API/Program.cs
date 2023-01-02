ccdbf4306000b3fb8771d282d6f124ea50cd7dac
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2023-01-02 09:27:18 +0530

    connect db and dependency injection

--- FullStack API/FullStack API/Program.cs
+++ FullStack API/FullStack API/Program.cs
@@ -1,3 +1,6 @@
+using FullStack_API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//dependency injection
+builder.Services.AddDbContext<FullStackDbContext>(options =>
+
+    options.UseSqlServer(builder.Configuration.GetConnectionString("FullStackConnectionString")
+));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
