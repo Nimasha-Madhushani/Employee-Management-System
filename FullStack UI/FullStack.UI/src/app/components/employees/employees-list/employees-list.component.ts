075ed5d032dcea2b69d3a993ed409455013d3f37
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2023-01-03 06:15:57 +0530

    inject employee service

--- FullStack UI/FullStack.UI/src/app/components/employees/employees-list/employees-list.component.ts
+++ FullStack UI/FullStack.UI/src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,43 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from 'src/app/models/employee.model';
+import { EmployeesService } from 'src/app/services/employees.service';
 
 @Component({
   selector: 'app-employees-list',
   templateUrl: './employees-list.component.html',
-  styleUrls: ['./employees-list.component.css']
+  styleUrls: ['./employees-list.component.css'],
 })
-export class EmployeesListComponent  implements OnInit{
-  employees:Employee[]=[
-    {
-      id:'aaaaaaaa',
-      name:'Nimasha1',
-      email:'n@gamil.com',
-      phone:273658965,
-      salary:120000,
-      department:'HR'
-    },
-    {
-      id:'bbbbbbbbb',
-      name:'Nimasha2',
-      email:'n@gamil.com',
-      phone:273658965,
-      salary:120000,
-      department:'HR'
-    },
-    {
-      id:'ccccccccc',
-      name:'Nimasha3',
-      email:'n@gamil.com',
-      phone:273658965,
-      salary:120000,
-      department:'HR'
-    }
+export class EmployeesListComponent implements OnInit {
+  employees: Employee[] = [
+    // {
+    //   id:'aaaaaaaa',
+    //   name:'Nimasha1',
+    //   email:'n@gamil.com',
+    //   phone:273658965,
+    //   salary:120000,
+    //   department:'HR'
+    // },
+    // {
+    //   id:'bbbbbbbbb',
+    //   name:'Nimasha2',
+    //   email:'n@gamil.com',
+    //   phone:273658965,
+    //   salary:120000,
+    //   department:'HR'
+    // },
+    // {
+    //   id:'ccccccccc',
+    //   name:'Nimasha3',
+    //   email:'n@gamil.com',
+    //   phone:273658965,
+    //   salary:120000,
+    //   department:'HR'
+    // }
   ];
-  constructor(){}
+  constructor(private employeesService: EmployeesService) {}
   ngOnInit(): void {
-
-    this.employees.push()
-    
+    // this.employees.push()
+    this.employeesService.getAllEmployees().subscribe({
+      next: (employees) => {
+        console.log(employees);
+      },
+      error: (response) => {
+        console.log(response);
+      },
+    });
   }
-
 }
--- FullStack UI/FullStack.UI/src/app/services/employees.service.ts
+++ FullStack UI/FullStack.UI/src/app/services/employees.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Employee } from '../models/employee.model';
+import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmployeesService {
+  baseApiUrl: string = environment.baseApiUrl;
+  constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient) { }
-
-  getAllEmployees(){
-this.http.get()
+  getAllEmployees():Observable<Employee[]> {
+    return this.http.get<Employee[]>(this.baseApiUrl + '/api/employees');
   }
 }
